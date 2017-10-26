277ae90dc1fd4498e127b7c06e617bdc4327974f
mahajanl <lakshamima@gmail.com>
2017-10-26 15:06:52 -0400

    learning how to integrate bootstrap into the page

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {   [Key]
+        public int ID { get; set; }
+
+        [MinLength(2),MaxLength(50)]
+        public string Title { get; set; }
+
+        [Display(Name ="Published Date")]
+        public DateTime PublishedDate { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+        
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category"), Display(Name ="Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
